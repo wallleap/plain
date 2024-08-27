611f352eab84e7be0d05f886498ebfb04669ed3c
wallleap <15579576761@163.com>
2024-08-27 16:38:37 +0800

    chore: 打包目录拆分

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,33 @@ export default defineConfig({
     vue(),
     UnoCSS(),
     viteCompression({
-      threshold: 1024000, // 对大于 1mb 的文件进行压缩
+      threshold: 300000,
     }),
   ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames(assetInfo) {
+          const imgExts = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp']
+          if (assetInfo.name?.endsWith('.css'))
+            return 'css/[name]-[hash].css'
+          if (imgExts.some(ext => assetInfo.name?.endsWith(ext)))
+            return 'imgs/[name]-[hash].[ext]'
+          return 'assets/[name]-[hash].[ext]'
+        },
+        manualChunks(id) {
+          if (id.includes('markdown')) {
+            if (id.includes('markdown-it/'))
+              return 'markdown-it'
+            else if (id.includes('node_modules'))
+              return 'markdown-chunk'
+            else return 'markdown'
+          }
+        },
+      },
+    },
+  },
 })
