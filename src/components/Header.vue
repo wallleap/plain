60068a2d7c07a53e5e006d0c355b336063e1f893
wallleap <15579576761@163.com>
2026-02-08 13:24:53 +0800

    feat(ui): 应用液态玻璃效果并重构头部导航栏样式
    - 为头部导航栏、返回顶部按钮及搜索/主题切换按钮添加液态玻璃效果
    - 移除原有路由激活状态的下划线指示器，改用背景高亮
    - 将头部导航栏改为固定定位并居中显示
    - 调整返回顶部按钮的样式与颜色以匹配新设计

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,6 +7,7 @@ import logo from '/logo.svg?raw'
 const isDark = ref(false)
 const themeStore = useThemeStore()
 const { t } = useI18n()
+const activeClass = 'liquid'
 const links = [
   {
     id: 1,
@@ -70,29 +71,40 @@ function changeTheme(event: MouseEvent) {
 </script>
 
 <template>
-  <header class="m-y-3 flex items-center justify-between line-height-none">
-    <a href="/" class="inline-block h-6 w-6 cursor-pointer hover:brightness-10 dark:hover:brightness-180">
-      <h1 class="m-0 h-100% p-0 align-middle font-size-2xl" v-html="logo" />
-    </a>
-    <nav class="m-0 flex">
-      <ul class="m-0 flex select-none list-none items-center justify-center gap-4 p-0 text-gray-500 all:transition-100">
-        <li v-for="link in links" :key="link.id" class="m-0 h-9 flex-shrink-0 cursor-pointer p-0">
-          <router-link :to="link.link" class="h-100% w-100% flex flex-justify-center flex-items-center rd-100 hover:text-gray-700 dark:hover:text-gray-200">
+  <header class="fixed left-0 right-0 top-2 z-999 mx-auto w-full flex select-none items-center justify-center line-height-none all:transition-200">
+    <nav class="liquid flex items-center justify-start rd-full bg-[rgba(0,0,0,0.05)] px-0.6 pb-0.7 pt-0.5 dark:bg-[rgba(255,255,255,0.09)]">
+      <a href="/" class="mr-8 inline-block h-5 w-5 cursor-pointer px-1 sm:px-4 xs:px-2 hover:brightness-10 dark:hover:brightness-180">
+        <h1 class="m-0 h-100% p-0 align-middle font-size-2xl" v-html="logo" />
+      </a>
+      <ul class="m-0 flex select-none list-none items-center justify-center p-0 text-gray-500 all:transition-100">
+        <li v-for="link in links" :key="link.id" class="m-0 flex flex-shrink-0 cursor-pointer items-center justify-center p-0">
+          <router-link
+            :to="link.link"
+            class="h-9 flex flex-justify-center flex-items-center rd-full px-1 text-black/75 sm:px-4 xs:px-2 dark:text-white"
+            :class="{ [activeClass]: $route.path === link.link }"
+            :style="{ backgroundColor: $route.path === link.link ? 'rgba(0,0,0,0.08)' : 'transparent' }"
+          >
             {{ link.name }}
           </router-link>
         </li>
       </ul>
-      <ul class="m-0 m-l-2 flex select-none list-none items-center justify-center p-0 text-gray-500 all:transition-100">
-        <li class="m-0 h-9 w-9 flex-shrink-0 cursor-pointer p-0">
-          <router-link to="/search" class="search-btn h-100% w-100% flex flex-justify-center flex-items-center rd-100 hover:text-gray-700 dark:hover:text-gray-200">
-            <i class="fa-solid fa-magnifying-glass" />
-          </router-link>
-        </li>
-        <li class="m-0 h-9 w-9 flex flex-shrink-0 cursor-pointer flex-justify-center flex-items-center rd-100 p-0 hover:text-gray-700 dark:hover:text-gray-200" @click="changeTheme">
-          <i v-if="!isDark" class="fa-solid fa-sun" />
-          <i v-else class="fa-solid fa-moon" />
-        </li>
-      </ul>
     </nav>
+    <ul
+      class="liquid m-l-2 h-auto w-auto flex list-none items-center justify-start overflow-hidden rd-full bg-[rgba(0,0,0,0.05)] px-0.6 pb-0.7 pt-0.5 transition-all duration-200 dark:bg-[rgba(255,255,255,0.09)]"
+      :class="{ 'w-9 h-9': $route.path === '/search' }"
+    >
+      <li class="m-0 cursor-pointer p-0">
+        <router-link
+          to="/search"
+          class="h-9 w-9 flex flex-justify-center flex-items-center rd-full text-black/75 dark:text-white"
+        >
+          <i class="fa-solid fa-magnifying-glass" />
+        </router-link>
+      </li>
+      <li class="m-0 h-9 w-9 flex cursor-pointer items-center justify-center rd-full p-0" @click="changeTheme">
+        <i v-if="!isDark" class="fa-solid fa-sun text-black/75 dark:text-white" />
+        <i v-else class="fa-solid fa-moon text-black/75 dark:text-white" />
+      </li>
+    </ul>
   </header>
 </template>
